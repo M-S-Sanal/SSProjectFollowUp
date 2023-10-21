756803b85031fc4aeeb66e4500380b7a4e6c64c9
Selman Şanal <114311575+MSSanal-UX@users.noreply.github.com>
2023-10-22 02:26:02 +0300

    AdminVM Created Bootstrap version updated

---
+++ ViewModels/AdminVM.cs
@@ -0,0 +1,11 @@
+﻿using SSProjectFollowUp.Models;
+
+namespace SSProjectFollowUp.ViewModels
+{
+    public class AdminVM
+    {
+        public int? compid { get; set; }
+        public Company? company { get; set; }
+
+    }
+}
